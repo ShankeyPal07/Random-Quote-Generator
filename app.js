3c4857695af532826714d2791ac6f2adedc8819a
shankeypal <130925236+ShankeyPal07@users.noreply.github.com>
2024-05-13 14:41:14 +0530

    update feat

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-let quoteEl = document.querySelector(".qoutes")
-console.log(quoteEl);
+let quoteEl = document.getElementById("qoutes");
+const url = "https://api.quotable.io/random";
 
-let qoutes = ["Carry out a random act of kindness, with no expectation of reward, safe in the knowledge that one day someone might do the same for you." , "The truth is that killing innocent people is always wrong - and no argument or excuse, no matter how deeply believed, can ever make it right. No religion on earth condones the killing of innocent people; no faith tradition tolerates the random killing of our brothers and sisters on this earth." , "I'm unpredictable, I never know where I'm going until I get there, I'm so random, I'm always growing, learning, changing, I'm never the same person twice. But one thing you can be sure of about me; is I will always do exactly what I want to do"]
+const fetchurl = async () => {
+  try {
+    const data = await fetch(url);
+    const res = await data.json();
 
+    
+    quoteEl.innerHTML = `
+    <p>${res.content}</p>
+    <span> - ${res.author}</span>`
+} catch (error) {}
+};
 
-function displayQuote() {
-
-   let num = ramdomNumber()
-        quoteEl.innerHTML = qoutes[num]
-        
- 
-}
-
-function ramdomNumber() {
-    return Math.floor(Math.random()*qoutes.length)
-}
-
+console.log(quoteEl)
 
