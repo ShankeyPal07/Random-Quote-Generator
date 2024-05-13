21b99661916c22bf481651589b5de210c94f2df8
shankeypal <130925236+ShankeyPal07@users.noreply.github.com>
2024-05-13 15:19:26 +0530

    update feat

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 let quoteEl = document.getElementById("qoutes");
 const url = "https://api.quotable.io/random";
+const btnEl = document.getElementById('btn')
 
 const fetchurl = async () => {
   try {
     const data = await fetch(url);
     const res = await data.json();
-
-    
     quoteEl.innerHTML = `
     <p>${res.content}</p>
     <span> - ${res.author}</span>`
 } catch (error) {}
 };
 
+btnEl.addEventListener('click' , fetchurl)
